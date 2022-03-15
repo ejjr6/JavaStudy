6f2cd7a4f36fbb8130f65827052fa1d498946459
ejjr6 <yej00725@gmail.com>
2022-03-15 22:09:07 +0900

    05~08

---
+++ 08_class/src/e/reference/Car.java
@@ -0,0 +1,19 @@
+package e.reference;
+
+public class Car {
+	String company;
+	String model;
+	
+	Engine engine;
+	
+	//Car(){}
+	
+	void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+}
+
+class Engine{
+	int maxSpeed;
+	int rpm;
+}
