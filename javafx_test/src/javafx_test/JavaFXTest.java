608b63761aceed374a43e7eecb5c04f3cd8137b5
ejjr6 <yej00725@gmail.com>
2022-03-28 10:49:49 +0900

    java_code_F

---
+++ javafx_test/src/javafx_test/JavaFXTest.java
@@ -0,0 +1,17 @@
+package javafx_test;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXTest extends Application {
+
+	@Override
+	public void start(Stage s) throws Exception {
+		s.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
